e28f530c8fade31c5f7c60a1b723f4aa9eb3f570
SReject <sreject@comcast.net>
2019-08-20 02:08:23 -0400

    Added unit tests for isUINT()

--- test/index.js
+++ test/index.js
@@ -3,6 +3,45 @@
 const assert = require('assert');
 const puzzles = require('./puzzles');
 const Sudoku = require('../');
+const {isUINT, getType} = require('../lib/misc/util.js');
+
+describe('toUINT()', function () {
+    it('Should return false if input is not numerical', function () {
+        assert.strictEqual(isUINT(null), false);
+        assert.strictEqual(isUINT(true), false);
+        assert.strictEqual(isUINT(false), false);
+        assert.strictEqual(isUINT('text'), false);
+        assert.strictEqual(isUINT([]), false);
+        assert.strictEqual(isUINT({}), false);
+    });
+    it('Should return false if the input is less than 0', function () {
+        assert.strictEqual(isUINT(-1), false);
+    });
+    it('Should return false if input is a decimal', function () {
+        assert.strictEqual(isUINT(1.2), false);
+    });
+    it('Should return true if input is equal to or greater than specified minimum', function () {
+        assert.strictEqual(isUINT(1, {min: 1}), true);
+        assert.strictEqual(isUINT(2, {min: 1}), true);
+    });
+    it('Should return false if input is below specified minimum', function () {
+        assert.strictEqual(isUINT(0, {min: 1}), false);
+    });
+    it('Should return true if input is equal to or less than specified maximum', function () {
+        assert.strictEqual(isUINT(2, {max: 2}), true);
+        assert.strictEqual(isUINT(1, {max: 2}), true);
+    });
+    it('Should return false if input is above specified maximum', function () {
+        assert.strictEqual(isUINT(2, {max: 1}), false);
+    });
+    it('Should return false if input falls outside of specified minimum and maximum ranges', function () {
+        assert.strictEqual(isUINT(3, {min: 4, max: 6}), false);
+        assert.strictEqual(isUINT(4, {min: 4, max: 6}), true);
+        assert.strictEqual(isUINT(5, {min: 4, max: 6}), true);
+        assert.strictEqual(isUINT(6, {min: 4, max: 6}), true);
+        assert.strictEqual(isUINT(7, {min: 4, max: 6}), false);
+    });
+});
 
 describe('SudokuSolver Class', function () {
     describe('#Constructor()', function () {
