7304eeeb3c449c1043c685342fd9c2dbfe75b5ef
SReject <sreject@comcast.net>
2019-08-20 01:20:33 -0400

    Adding more unit tests

--- test/index.js
+++ test/index.js
@@ -7,46 +7,74 @@ const Sudoku = require('..');
 
 describe('SudokuSolver Class', function () {
     describe('#Constructor()', function () {
-        describe('Should require', function () {
-            it('An array as the puzzle input', function () {
+        describe('Puzzle inputs', function () {
+            it('must be an array', function () {
                 assert.throws(() => new Sudoku());
             });
-
-            it('Puzzles to be a square', function () {
+            it('must be square', function () {
                 assert.throws(() => new Sudoku(puzzles['Non-Square Puzzle']));
             });
-
-            it('All given cell values to be unsigned integer', function () {
+            it('must only have cell values that are unsigned integers', function () {
                 assert.throws(() => new Sudoku(puzzles['Invalid:Decimal']));
                 assert.throws(() => new Sudoku(puzzles['Invalid:String']));
                 assert.throws(() => new Sudoku(puzzles['Invalid:NumericString']));
             });
 
-            it('All cell values to be no greater than the number of cells in a group', function () {
+            it('must not have any cell value greater than the number of cells in a group', function () {
                 assert.throws(() => new Sudoku(puzzles['Invalid:ValueToLarge']));
             });
         });
 
-        describe('Should be able to create an instance', function () {
+        describe('Sector-size inputs', function () {
+            let puzzle = puzzles['Empty Puzzle'];
+            it('May be omitted', function () {
+                new Sudoku(puzzle);
+            });
 
+            it('May be numerical', function () {
+                new Sudoku(puzzle, 3);
+            });
+
+            it('May be an empty object', function () {
+                new Sudoku(puzzle, {});
+            });
+
+            it('May be an object with only a width specified', function () {
+                new Sudoku(puzzle, {width: 3});
+            });
+
+            it('May be an object with only a height specified', function () {
+                new Sudoku(puzzle, {height: 3});
+            });
+
+            it('May be an object that specifies both width and height', function () {
+                new Sudoku(puzzle, {width: 3, height: 3});
+            });
+
+            it('Must divide the group-length evenly', function () {
+                assert.throws(() => new Sudoku(puzzle, 4));
+            });
+
+        });
+
+        describe('Should be able to create an instance', function () {
             it('With no solved cells', function () {
                 new Sudoku(puzzles['Empty Puzzle']);
             });
-
             it('With at least one cell solved', function () {
                 new Sudoku(puzzles['1 Solved Cell']);
             });
-
             it('With all cells solved', function () {
                 new Sudoku(puzzles['Solved Puzzle']);
             });
         });
 
+
+
         describe('Should be able to create non-traditional sized puzzles', function () {
             it('25x25 with assumed sector size', function () {
                 new Sudoku(puzzles['25x25 Puzzle']);
             });
-
             it('10x10 with specified sector size', function () {
                 new Sudoku(puzzles['10x10 Puzzle'], {width: 5, height: 5});
             });
